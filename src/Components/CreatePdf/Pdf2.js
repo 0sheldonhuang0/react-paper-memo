c822e441cbc88a14629b4715fa06a97f86ae1452
0sheldonhuang0 <sheldonhuang@qq.com>
2020-04-15 21:54:50 +0200

    Update Pdf2.js

--- src/Components/CreatePdf/Pdf2.js
+++ src/Components/CreatePdf/Pdf2.js
@@ -14,14 +14,59 @@ export default function Pdf2(uploadData) {
   var verticalSegmentText = paperA4[0] / 4;
   var horizonSegmentText = paperA4[1] / 2;
 
-  //设置分割线（虚线）
-  doc.setLineDash([1]);
-  doc.line(5, verticalSegment, 205, verticalSegment);
-
   //设置内容位置
   console.log(uploadData);
-  doc.text(uploadData, horizonSegmentText, verticalSegmentText, "center");
-  doc.text(uploadData, horizonSegmentText, 3 * verticalSegmentText, "center");
+  let displayUploadData = uploadData.split("\n"); //将文件内容根据换行符隔开，["A-B","A-B","A-B","A-B"...]
+  let displayUploadDataA = []; //正面内容
+  let displayUploadDataB = []; //反面内容
+
+  if (displayUploadData.length % 2 != 0) {
+    displayUploadData.push(" - ");
+  }
+
+  for (let i = 0; i < displayUploadData.length; i++) {
+    let displayUploadDataTemp = displayUploadData[i].split("-"); //暂时储存一张卡片正反面，["A","B"]
+    displayUploadDataA.push(displayUploadDataTemp[0]); //["A","A","A","A","A"...]
+    displayUploadDataB.push(displayUploadDataTemp[1]); //["B","B","B","B","B"...]
+  }
+
+  for (let i = 0; i < displayUploadData.length - 1; i = i + 2) {
+    //设置分割线（虚线）
+    doc.setLineDash([1]);
+    doc.line(5, verticalSegment, 205, verticalSegment);
+
+    doc.text(
+      displayUploadDataA[i],
+      horizonSegmentText,
+      verticalSegmentText,
+      "center"
+    );
+    doc.text(
+      displayUploadDataA[i + 1],
+      horizonSegmentText,
+      3 * verticalSegmentText,
+      "center"
+    );
+    doc.addPage("a4");
+    //设置分割线（虚线）
+    doc.setLineDash([1]);
+    doc.line(5, verticalSegment, 205, verticalSegment);
+
+    doc.text(displayUploadDataB[i], horizonSegmentText, verticalSegmentText, {
+      align: "center",
+      maxWidth: "105",
+    });
+    doc.text(
+      displayUploadDataB[i + 1],
+      horizonSegmentText,
+      3 * verticalSegmentText,
+      { align: "center", maxWidth: "105" }
+    );
+    doc.addPage("a4");
+    //设置分割线（虚线）
+    doc.setLineDash([1]);
+    doc.line(5, verticalSegment, 205, verticalSegment);
+  }
 
   doc.save("a4.pdf");
 }
